568eb98631245787aa37733c295ed140eddd3d1f
Marwan Sabry <112923720+MarwanVII@users.noreply.github.com>
2024-03-05 15:55:28 +0200

    Added First Algorithm

---
+++ caesar-cipher.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string encrypt(string output, int key)
+{
+	for(auto &c:output)
+	{
+		if(!isalpha(c))
+			continue;
+		if(islower(c))
+		{
+			int dif = (c - 'a' + key)%26;
+			c = 'a' + dif;
+		}
+		else if(isupper(c))
+		{
+			int dif = (c - 'A' + key)%26;
+			c = 'A' + dif;
+		}
+	}
+	return output;
+}
+string decrypt(string output, int key)
+{
+	for(auto &c:output)
+	{
+		if(!isalpha(c))
+			continue;
+		if(islower(c))
+		{
+			int dif = abs(c - 'a' - key + 26)%26;
+			c = 'a' + dif;
+		}
+		else if(isupper(c))
+		{
+			int dif = abs(c - 'A' - key + 26)%26;
+			c = 'A' + dif;
+		}
+	}
+	return output;
+}
+int main() {
+	string s;
+	int key;
+	cout << "Enter a text: ";
+	getline(cin,s);
+	cout << "Enter a key: ";
+	cin >> key;
+	string encrypted = encrypt(s,key);
+	cout << "Encrypted: " << encrypted << endl;
+	cout << "Decrypted: " << decrypt(encrypted,key) << endl;
+	return 0;
+}
